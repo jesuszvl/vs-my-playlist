b6f4201aed4c1280f4d581532ec40825f89810e0
jesuszavalagt <jesus.zavala@gametime.co>
2022-11-18 22:16:12 -0800

    create track card, load track info and play preview on click

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import axios from "axios";
+import { Howl } from "howler";
 
-const DEFAULT_TITLE = "Versus My Playlist";
+const TITLES = {
+  NAME: "Versus My Playlist",
+  PLAYLIST: "Chose one playlist",
+  TRACK: "Pick the better song",
+};
 
 function App() {
   const CLIENT_ID = "ad1d9256da1648fe842417e4533e59e8";
@@ -10,11 +15,12 @@ function App() {
   const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
   const RESPONSE_TYPE = "token";
   const SCOPE = "playlist-read-private";
+  const MINIMUM_PLAYLIST_SIZE = 25;
 
   const [token, setToken] = useState("");
   const [playlists, setPlaylists] = useState([]);
   const [tracks, setTracks] = useState([]);
-  const [title, setTitle] = useState(DEFAULT_TITLE);
+  const [title, setTitle] = useState(TITLES.NAME);
   const [backToPlaylist, setBackToPlaylist] = useState(false);
 
   useEffect(() => {
@@ -36,13 +42,6 @@ function App() {
     setToken(token);
   }, []);
 
-  const logout = () => {
-    setPlaylists([]);
-    setToken("");
-    setTitle(DEFAULT_TITLE);
-    window.localStorage.removeItem("token");
-  };
-
   const getUserPlaylists = async (token) => {
     const { data } = await axios.get(
       "https://api.spotify.com/v1/me/playlists",
@@ -58,15 +57,26 @@ function App() {
 
     const filteredPlaylists = data.items.filter(
       (playlist) =>
-        playlist.tracks.total >= 30 && playlist.owner.id !== "spotify"
+        playlist.tracks.total >= MINIMUM_PLAYLIST_SIZE &&
+        playlist.owner.id !== "spotify"
     );
 
     const sortedPlaylist = filteredPlaylists.sort(
-      (a, b) => a.tracks.total - b.tracks.total
+      (a, b) => b.tracks.total - a.tracks.total
     );
 
     setPlaylists(sortedPlaylist);
-    setTitle("Chose one playlist to sort");
+    setTitle(TITLES.PLAYLIST);
+  };
+
+  const getPlaylistTracks = async (playlist) => {
+    const { data } = await axios.get(playlist.tracks.href, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    setTracks(data.items);
+    setBackToPlaylist(true);
   };
 
   const renderPlaylists = () => {
@@ -84,59 +94,74 @@ function App() {
     });
   };
 
-  const onPlaylistClick = (playlist) => {
-    getPlaylistTracks(playlist);
-    setTitle("Pick the better song");
-  };
-
-  const getPlaylistTracks = async (playlist) => {
-    const { data } = await axios.get(playlist.tracks.href, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    setTracks(data.items);
-    setBackToPlaylist(true);
-  };
-
-  const onTrackClick = (trackId) => {
-    console.log(trackId);
-  };
-
   const renderTracks = () => {
     const max = tracks.length;
     const min = 2;
     const a = Math.floor(Math.random() * (max - min + 1)) + min;
-    return tracks.slice(a - 2, a).map((track) => (
-      <div key={track.track.id}>
-        {track.track.album.images.length ? (
+
+    return tracks.slice(a - 2, a).map((track) => {
+      const trackObj = track.track;
+      return (
+        <div
+          key={trackObj.id}
+          className="track"
+          onClick={() => onTrackClick(trackObj)}
+        >
           <img
-            width={"150px"}
-            src={track.track.album.images[0].url}
-            alt=""
-            onClick={() => onTrackClick(track.track.id)}
+            className="track-image"
+            src={trackObj.album.images[0].url}
+            alt={trackObj.name}
           />
-        ) : (
-          <div>No Image</div>
-        )}
-        <p>{track.track.name}</p>
-      </div>
-    ));
+          <div className="track-info">
+            <p className="track-name">{trackObj.name}</p>
+            <p className="track-artist">{trackObj.artists[0].name}</p>
+            <p className="track-artist">{trackObj.popularity}</p>
+          </div>
+        </div>
+      );
+    });
+  };
+
+  const onPlaylistClick = (playlist) => {
+    getPlaylistTracks(playlist);
+    setTitle(TITLES.TRACK);
+  };
+
+  const onTrackClick = (track) => {
+    console.log(track);
+    const sound = new Howl({
+      src: [track.preview_url],
+      html5: true,
+    });
+    sound.play();
   };
 
   const backToPlaylistClick = () => {
     setTracks([]);
     setBackToPlaylist(false);
+    setTitle(TITLES.PLAYLIST);
   };
+
+  const logout = () => {
+    setPlaylists([]);
+    setToken("");
+    setTitle(TITLES.NAME);
+    window.localStorage.removeItem("token");
+  };
+
+  const shouldRenderPlaylists =
+    playlists.length > 0 && tracks.length === 0 && token;
+
+  const shouldRenderTracks = tracks.length !== 0 && token;
 
   return (
     <div className="App">
       <header className="App-header">
         <h2>{title}</h2>
-        {playlists.length > 0 && tracks.length === 0 && token && (
+        {shouldRenderPlaylists && (
           <div className="playlists-container">{renderPlaylists()}</div>
         )}
-        {tracks.length !== 0 && token && (
+        {shouldRenderTracks && (
           <div className="tracks-container">{renderTracks()}</div>
         )}
         {!token ? (
@@ -147,7 +172,7 @@ function App() {
             Login with Spotify
           </a>
         ) : (
-          <>
+          <div className="action-buttons">
             {backToPlaylist && (
               <button className="spotify-logout" onClick={backToPlaylistClick}>
                 Back To Playlists
@@ -156,7 +181,7 @@ function App() {
             <button className="spotify-logout" onClick={logout}>
               Logout
             </button>
-          </>
+          </div>
         )}
       </header>
     </div>
